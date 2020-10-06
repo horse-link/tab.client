749b9170c203b21c945cd2d1338f9f7206bcf54d
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2020-10-06 16:28:17 +1000

    Updates

--- tab.client/Models/Transactions/Details.cs
+++ tab.client/Models/Transactions/Details.cs
@@ -6,7 +6,16 @@ namespace tab.client.Models.Transactions
 {
     public class Details 
     {
+        [JsonProperty("tsn")]
+        public String TSN { get; set; }
+
         [JsonProperty("result")]
         public String Result { get; set; }
+
+        [JsonProperty("transactionReference")]
+        public String TransactionReference { get; set; }
+
+        [JsonProperty("transactionTime")]
+        public DateTime TransactionTime { get; set; }
     }
 }
